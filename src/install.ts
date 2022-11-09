58eca1a06b63fd4e9d793fc16f990370a93f0b6c
mattbr0wn <matt@openline.ai>
2022-11-09 22:15:30 +0000

    install script

--- src/install.ts
+++ src/install.ts
@@ -1,5 +1,17 @@
 const shell = require('shelljs');
 
 export function installWebsite(): void {
+    
+    if (!shell.which('git')) {
+        shell.echo('Sorry, this script requires git');
+        shell.exit(1);
+    }
+    
     shell.echo('installing website...');
+    const path = '/code/openline-website'
+    shell.mkdir(path)
+    shell.cd(path)
+    shell.exec('git clone https://github.com/openline-ai/openline.ai.git')
+    shell.exec('yarn install')
+    shell.exec('yarn start')
 };
