7653bd949c7960810d32e565e8bf30e2a186b67b
mattbr0wn <matt@openline.ai>
2022-11-09 21:54:32 +0000

    shell program

--- src/cli.ts
+++ src/cli.ts
@@ -5,6 +5,9 @@ const clear = require('clear');
 const figlet = require('figlet');
 const path = require('path');
 const program = require('commander');
+const shell = require('shelljs')
+
+import * as appInstall from './install';
 
 clear();
 console.log(
@@ -13,3 +16,34 @@ console.log(
     )
 );
 
+console.log(' ');
+console.log(' ');
+
+program
+    .name('Openline CLI')
+    .version('0.0.1')
+    .description('CLI for the Openline ecosystem')
+    .option('-i, --install <app name>', 'install an Openline application')
+    .option('-r, --run <app name>', 'run an Openline application')
+
+program.parse(process.argv);
+
+const options = program.opts();
+if (options.debug) {
+    console.log(options);
+    console.log('');
+};
+
+if(options.install) {
+    let app = options.install;
+    if(app == 'website') {
+        appInstall.installWebsite()
+    };
+    //console.log(` Install ${options.install}`);
+};
+
+if(options.run) console.log(`Run ${options.run}`);
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
