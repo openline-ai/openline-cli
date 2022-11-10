7937e678b44cd93d3edbca10a6f4f982193b2a42
mattbr0wn <matt@openline.ai>
2022-11-10 21:04:56 +0000

    update scripts

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -4,12 +4,13 @@ set -e
 
 VERSION=18.12.1
 
-mkdir -p /usr/local/lib/node
+sudo mkdir -p /usr/local/lib/node
 wget "https://nodejs.org/dist/v${VERSION}/node-v${VERSION}-linux-x64.tar.xz"
-tar -C /usr/local -xzf "node-v${VERSION}-linux-x64.tar.xz"
+sudo tar -xJvf "node-v${VERSION}-linux-x64.tar.xz"
 rm "node-v${VERSION}-linux-x64.tar.xz"
+cd node-v${VERSION}-linux-x64
+sudo cp -r ./{lib,share,include,bin} /usr
 
-export NODEJS_HOME=/usr/local/lib/node
-export PATH=$NODEJS_HOME/bin:$PATH
+export PATH=/usr/bin:$PATH
 
-. ~/.profile
+. ~/.bash_profile
