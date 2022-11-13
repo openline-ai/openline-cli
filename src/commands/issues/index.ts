6ae8e1618eb2e5a552117234d8a25f87188cce34
mattbr0wn <matt@openline.ai>
2022-11-13 20:21:04 +0000

    feat: milestone flag

--- src/commands/issues/index.ts
+++ src/commands/issues/index.ts
@@ -15,9 +15,10 @@ export default class Issue extends Command {
   ]
   
   static flags = {
-    me: Flags.boolean({char: 'm', summary: 'issues assigned to me', description: 'list of issues assigned to me'}),
-    new: Flags.boolean({char: 'n', summary: 'create new github issue', description: 'create a new github issue against an openline repo'}),
-    repo: Flags.string({char: 'r', summary: 'openline-ai repo', description: 'the openline repo that cooresponds with the openline product', options: ['cli', 'customer-os', 'contacts', 'oasis', 'website']}),
+    bug: Flags.boolean({char: 'b', description: 'return all bugs assigned to me'}),
+    milestone: Flags.boolean({char: 'm', description: 'return all issues tageed to a milestone assigned to me'}),
+    //new: Flags.boolean({char: 'n', summary: 'create new github issue', description: 'create a new github issue against an openline repo'}),
+    //repo: Flags.string({char: 'r', summary: 'openline-ai repo', description: 'the openline repo that cooresponds with the openline product', options: ['cli', 'customer-os', 'contacts', 'oasis', 'website']}),
   }
   
   static args = []
@@ -26,43 +27,28 @@ export default class Issue extends Command {
     const {args, flags} = await this.parse(Issue)
     
     // return a list of issues assigned to me across all repos
-    if (flags.me) {
-      const octokit = new Octokit({
-        auth: process.env.GITHUB_TOKEN,
-      })
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    })
 
-      let resp = await octokit.request('GET /orgs/{org}/issues', {
-        org: 'openline-ai',
-      })
+    let resp = await octokit.request('GET /orgs/{org}/issues', {
+      org: 'openline-ai',
+    })
 
-      console.log('')
-      console.log(figlet.textSync('My issues...'))
-      console.log('')
+    console.log('')
+    console.log(figlet.textSync('My issues...'))
+    console.log('')
 
-      let issueCount = 0
+    let issueCount = 0
 
-      resp.data.forEach(issue => {
+    resp.data.forEach(issue => {
+      if (flags.milestone && issue.milestone?.title) {
+        printIssue(issue)
         issueCount++
-        let url = issue.html_url
-        let issueNumber = issue.number
-        let issueId = issue.id
-        let title = issue.title
-        //let labels: string[] = []
-        let milestone = issue.milestone?.title
-        let created = issue.created_at
-        //let comments = issue.comments
-        let repository = issue.repository?.name
-
-
-        console.log(repository, ' #', issueNumber, ' ', title, ' - ', issueId)
-        console.log(url)
-        //console.log('Labels: ', labels)
-        console.log('Milestones: ', milestone)
-        console.log('Created: ', created)
-        console.log('')
-      })
-      console.log('Your open issues: ', issueCount)
-    }
+      }
+    })
+    console.log('Your open issues: ', issueCount)
+    
 
     // create a new issue
     if (flags.new) {
@@ -71,4 +57,28 @@ export default class Issue extends Command {
       console.log('New issue against ', flags.repo) 
     }
   }
+}
+
+function printIssue(issue: any) {
+  let labels :string[] = []
+  if (issue.labels) {
+    issue.labels.forEach((l: { name: string }) => {
+      labels.push(l.name)
+    })
+  }
+  
+  let url = issue.html_url
+  let issueNumber = issue.number
+  let issueId = issue.id
+  let title = issue.title
+  let milestone = issue.milestone?.title
+  let created = issue.created_at
+  let repository = issue.repository?.name
+
+  console.log(repository, ' #', issueNumber, ' ', title, ' - ', issueId)
+  console.log(url)
+  console.log('Labels: ', labels)
+  console.log('Milestones: ', milestone)
+  console.log('Created: ', created)
+  console.log('')
 }
