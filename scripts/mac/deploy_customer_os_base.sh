0ae4dc03465d50c408756817eff3f68297b3153d
mattbr0wn <matt@openline.ai>
2022-11-14 17:03:44 +0000

    feat: mac scripts

---
+++ scripts/mac/deploy_customer_os_base.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+MINIKUBE_STATUS=$(minikube status)
+MINIKUBE_STARTED_STATUS_TEXT='Running'
+if [[ "$MINIKUBE_STATUS" == *"$MINIKUBE_STARTED_STATUS_TEXT"* ]];
+  then
+    echo "👍 Minikube already started"
+  else
+    eval $(minikube docker-env)
+    minikube start &
+    if [ $? -eq 0 ]; then
+        echo "✅ minikube started"
+    else
+        echo "❌ minikube failed to start"
+    fi
+     
+fi
+
+NAMESPACE_NAME="openline"
+CUSTOMER_OS_HOME="$(dirname $(readlink -f $0))/../../../"
+echo CUSTOMER_OS_HOME=$CUSTOMER_OS_HOME
+
+if [[ $(kubectl get namespaces) == *"$NAMESPACE_NAME"* ]];
+  then
+    echo " --- Continue deploy on namespace $NAMESPACE_NAME --- "
+  else
+    echo " --- Creating $NAMESPACE_NAME namespace in minikube ---"
+    kubectl create -f "$CUSTOMER_OS_HOME/deployment/k8s/configs/openline-namespace.json"
+    if [ $? -eq 0 ]; then
+        echo "✅ $NAMESPACE_NAME namespace created in minikube"
+    else
+        echo "❌ failed to create $NAMESPACE_NAME namespace in minikube"
+    fi
+    
+fi
+
+#Adding helm repos :
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add neo4j https://helm.neo4j.com/neo4j
+helm repo add fusionauth https://fusionauth.github.io/charts
+helm repo update
+echo "✅ helm repo's added"
+
+#install postgresql
+kubectl apply -f $CUSTOMER_OS_HOME/deployment/k8s/configs/postgresql/postgresql-presistent-volume.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f $CUSTOMER_OS_HOME/deployment/k8s/configs/postgresql/postgresql-persistent-volume-claim.yaml --namespace $NAMESPACE_NAME
+
+helm install --values "$CUSTOMER_OS_HOME/deployment/k8s/configs/postgresql/postgresql-values.yaml" postgresql-customer-os-dev bitnami/postgresql --namespace $NAMESPACE_NAME
+if [ $? -eq 0 ]; then
+    echo "✅ postgreSQL installed"
+else
+    echo "❌ failed to install postgreSQL"
+fi
+
+
+helm install neo4j-customer-os neo4j/neo4j-standalone --set volumes.data.mode=defaultStorageClass -f $CUSTOMER_OS_HOME/deployment/k8s/configs/neo4j/neo4j-helm-values.yaml --namespace $NAMESPACE_NAME
+if [ $? -eq 0 ]; then
+    echo "✅ Neo4j installed"
+else
+    echo "❌ failed to install Neo4j"
+fi
+
+helm install fusionauth-customer-os fusionauth/fusionauth -f "$CUSTOMER_OS_HOME/deployment/k8s/configs/fusionauth/fusionauth-values.yaml" --namespace $NAMESPACE_NAME
+if [ $? -eq 0 ]; then
+    echo "✅ FusionAuth installed"
+else
+    echo "❌ failed to install FusionAuth"
+fi
---
+++ scripts/mac/install_docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ $(docker --version) == *"Docker version"* ]];
+    then
+        echo "--- Docker already installed 👍 ---"
+    else
+        echo "Installing Docker..."
+        softwareupdate --install-rosetta
+        
+        if [[ $(arch) == 'arm54' ]]; 
+            then
+                curl -L https://desktop.docker.com/mac/main/arm64/Docker.dmg --output Docker.dmg
+            else
+                curl -L https://desktop.docker.com/mac/main/amd64/Docker.dmg --output Docker.dmg
+        fi
+
+        sudo hdiutil attach Docker.dmg
+        sudo /Volumes/Docker/Docker.app/Contents/MacOS/install
+        sudo hdiutil detach /Volumes/Docker
+
+        echo "✅ Docker install completed."
+        echo "❗️Please open Docker desktop via the GUI to initialize the application before proceeding."
+fi
+
+
+
+
+
+
