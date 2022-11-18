e4dd492ab84b002e559c494e964c2284323335c5
mattbr0wn <matt@openline.ai>
2022-11-18 10:34:30 +0000

    update dev commands

--- src/commands/dev/index.ts
+++ src/commands/dev/index.ts
@@ -13,24 +13,24 @@ export default class Dev extends Command {
   ]
 
   static flags = {
-    start: Flags.string({
-      description: 'start openline application', 
-      options: ['all', 'customer-os', 'oasis', 'contacts'],  
-      required: false,
-    }),
-    stop: Flags.boolean({
-      description: 'stop openline applications', 
-      aliases: ['kill', 'k'], 
-      required: false,
+    kill: Flags.boolean({
+      char: 'k',
+      description: 'kill all openline services'
     }),
     ping: Flags.string({
       description: 'service health check',
       char: 'p', 
       options: ['all', 'customer-os', 'oasis', 'contacts'], 
-      required: false,
+    }),
+    start: Flags.string({
+      description: 'start openline application', 
+      options: ['all', 'customer-os', 'oasis', 'contacts'],  
     }),
     status: Flags.boolean({
       description: 'current status of kubernetes cluser',
+    }),
+    stop: Flags.boolean({
+      description: 'stop openline applications', 
     })
   }
 
@@ -40,14 +40,64 @@ export default class Dev extends Command {
     const {args, flags} = await this.parse(Dev)
 
     if ((flags.start) && (flags.stop)) {
-      console.log('Error:  only one flag can be set at a time')
+      console.log('❌ Error:  only one flag can be set at a time')
     }
     else if (flags.start == 'customer-os') {
-      startCustomerOs()
+      let isRunning: boolean = runningCheck()
+      if (isRunning == false) {
+        console.log('🦦 starting customerOS...')
+        let startCode = shell.exec('colima start --with-kubernetes --cpu 2 --memory 4 --disk 60', {silent: true})
+        if (startCode.code == 0) {
+          console.log('✅ Colima running')
+          
+          let isInstalled: boolean = installCheck()
+          if (isInstalled == false) {
+            console.log('🦦 installing customerOS...')
+            installCustomerOs()
+          } 
+          else {
+            console.log('✅ customerOS is running...')
+          }
+        }
+        else {
+          console.log('❌ Colima failed to start')
+          console.log(startCode.stderr)
+        }
+      }
+      else {
+        let isInstalled: boolean = installCheck()
+        if (isInstalled == false) {
+          console.log('🦦 installing customerOS...')
+          installCustomerOs()
+          } 
+        else {
+          console.log('✅ customerOS is running...')
+        }
+      }
     }
+
     else if (flags.stop) {
-      shell.exec('y | colima delete')
-      shell.exec('colima stop')
+      let stopCode = shell.exec('colima stop', {silent:true})
+      if (stopCode.code == 0) {
+        console.log('✅ All Openline services have been stopped')
+        console.log('✅ Configuration has been saved and will be re-applied on next restart')
+      }
+      else {
+        console.log('❌ problem stopping Openline services')
+        console.log(stopCode.stderr)
+      }
+    }
+      
+    else if (flags.kill) {
+      shell.exec('y | colima delete', {silent: true})
+      let killcode = shell.exec('colima stop', {silent: true})
+      if (killcode.code == 0) {
+        console.log('✅ All Openline services have been stopped and deleted')
+      }
+      else {
+        console.log('❌ Problem killing openline services')
+        console.log(killcode.stderr)
+      }
     }
     
     else if (flags.ping == 'customer-os') {
@@ -63,18 +113,29 @@ export default class Dev extends Command {
     }
     
     else if (flags.status) {
-      console.log('🦦 k8s pods')
-      shell.exec('kubectl get pods -n openline')
-      console.log('')
-      console.log('🦦 k8s persistent volume claims')
-      shell.exec('kubectl get pvc -n openline')
-      console.log(installCheck())
+      let isUp = shell.exec('kubectl get services', {silent: true})
+      if (isUp.code == 0) {
+        console.log('🦦 k8s cluster')
+        shell.exec('kubectl get services')
+        console.log('')
+        console.log('🦦 k8s pods: openline')
+        shell.exec('kubectl get pods -n openline -o wide')
+        console.log('🦦 k8s pods: kube-system')
+        shell.exec('kubectl get pods -n kube-system -o wide')
+        console.log('')
+        console.log('🦦 k8s persistent volumes')
+        shell.exec('kubectl get pv')
+      }
+      else {
+        console.log('❌ Openline services are not running')
+        console.log('🦦 Please run [openline dev --start] to start services')
+      }
     }
   }
 }
 
-function installCheck() :boolean {
-  if (shell.exec('kubectl get ns openline').code == 0) {
+function runningCheck() :boolean {
+  if (shell.exec('colima status', {silent: true}).code == 0) {
     return true
   }
   else {
@@ -82,7 +143,16 @@ function installCheck() :boolean {
   }
 }
 
-function startCustomerOs() {
+function installCheck() :boolean {
+  if (shell.exec('kubectl get ns openline', {silent: true}).code == 0) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+function installCustomerOs() {
   let osType: string = process.platform
   var depend = ''
   if (osType == 'darwin') {
