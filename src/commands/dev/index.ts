56d184c6804dcddf94a933010ea30acccb7a7c25
mattbr0wn <matt@openline.ai>
2022-11-19 16:39:37 +0000

    added topics

--- src/commands/dev/index.ts
+++ src/commands/dev/index.ts
@@ -1,8 +1,7 @@
 import {Command, Flags} from '@oclif/core'
 import {getHelpFlagAdditions} from '@oclif/core/lib/help'
-import { start } from 'repl'
 import * as shell from 'shelljs'
-import {dependencies} from './mac'
+import {dependencies} from '../../checks/mac'
 
 export default class Dev extends Command {
   static description = 'starts and stops local development server for openline applications'
@@ -14,7 +13,7 @@ export default class Dev extends Command {
     'openline dev -p customer-os',
   ]
 
-  static flags = {
+  /*static flags = {
     kill: Flags.boolean({
       char: 'k',
       description: 'kill all openline services'
@@ -42,8 +41,7 @@ export default class Dev extends Command {
 
   static args = []
 
-  public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Dev)
+
 
     if ((flags.start) && (flags.stop)) {
       console.log('❌ Error:  only one flag can be set at a time')
@@ -141,52 +139,8 @@ export default class Dev extends Command {
     }
   }
 }
-
-function runningCheck() :boolean {
-  if (shell.exec('colima status', {silent: true}).code == 0) {
-    return true
-  }
-  else {
-    return false
-  }
-}
-
-function installCheck() :boolean {
-  if (shell.exec('kubectl get ns openline', {silent: true}).code == 0) {
-    return true
-  }
-  else {
-    return false
-  }
-}
-
-
-function installCustomerOs(verbose: boolean) {
   
-  let osType: string = process.platform
-  var depend = ''
-  if (osType == 'darwin') {
-    dependencies(!verbose)
-  } 
-  else {
-    console.log('Your OS is currently unsupported :(')
-    process.exit(1)
-  }
-
-  let getConfig = 'https://raw.githubusercontent.com/openline-ai/openline-customer-os/otter/deployment/scripts/0-get-config.sh'
-  let configErr = shell.exec(`curl -sL ${getConfig} | bash`, {silent: verbose}).code
-  
-  let dependErr = shell.exec(`curl -sL ${depend} | bash`, {silent: verbose}).code
-
-  let baseInstall = 'https://raw.githubusercontent.com/openline-ai/openline-customer-os/otter/deployment/scripts/2-install.sh'
-  let baseErr = shell.exec(`curl -sL ${baseInstall} | bash`, {silent: verbose}).code
-  
-  let deploy = 'https://raw.githubusercontent.com/openline-ai/openline-customer-os/otter/deployment/scripts/3-db-setup.sh'
-  let deployErr = shell.exec(`curl -sL ${deploy} | bash`, {silent: verbose}).code
-  
-  console.log('')
-  console.log('✅ customerOS successfully started!')
-  console.log('🦦 To validate the service is reachable run the command =>  openline dev --ping customer-os')
-  console.log('🦦 Visit http://localhost:10010 in your browser to play around with the graph API explorer')
-  shell.exec('open http://localhost:10010')
+*/
+public async run(): Promise<void> {
+  const {args, flags} = await this.parse(Dev)}
 }
