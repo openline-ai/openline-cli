9344a0201b9d5bfb72b6ea37a5021b959027d7d1
mattbr0wn <matt@openline.ai>
2022-11-20 21:23:22 +0000

    moved values to config

--- src/commands/dev/start.ts
+++ src/commands/dev/start.ts
@@ -2,6 +2,7 @@ import {Command, Flags, CliUx} from '@oclif/core'
 import * as shell from 'shelljs'
 import * as dev from '../../actions/devServer'
 import * as mac from '../../checks/mac'
+import * as config from 'config'
 
 
 export default class DevStart extends Command {
@@ -28,18 +29,23 @@ export default class DevStart extends Command {
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(DevStart)
+
+    let verbose = flags.verbose
+    if (config.has('verbose')) {
+      verbose = config.get('verbose')
+    }
     
     // Base dependency check
-    let depend = mac.dependencies(flags.verbose)
+    let depend = mac.dependencies(verbose)
     if (!depend) {
       this.exit(1)
     }
 
     this.log('🦦 initiating Openline dev server...')
-    let start = dev.startColima(flags.verbose)
+    let start = dev.startColima(verbose)
     if (start) {
       this.log('🦦 installing customerOS...this may take a few mins')
-      let customerOs = dev.installCustomerOs(flags.verbose)
+      let customerOs = dev.installCustomerOs(verbose)
       if (customerOs) {
         this.log('')
         this.log('✅ customerOS started successfully!')
