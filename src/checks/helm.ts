868e5925f448b86cd6d0ec7048cb8a9371919a31
mattbr0wn <matt@openline.ai>
2022-11-20 21:04:54 +0000

    moved values into config

--- src/checks/helm.ts
+++ src/checks/helm.ts
@@ -1,4 +1,5 @@
 import * as shell from 'shelljs'
+import * as config from 'config'
 
 export function helmCheck() :boolean {
     let result = shell.exec('which helm', {silent: true})
@@ -11,7 +12,8 @@ export function helmCheck() :boolean {
 }
 
 export function installHelm() :boolean {
-    let result = shell.exec('brew install helm')
+    let install: string = config.get('dependencies.helm.mac')
+    let result = shell.exec(install)
     if (result.code == 0) {
         return true
     } else {
