035257cca05114196b28b3d01fc54cc1eb4cebb1
mattbr0wn <matt@openline.ai>
2022-11-22 16:57:56 +0000

    updated error handling

--- src/lib/dev/errors.ts
+++ src/lib/dev/errors.ts
@@ -3,9 +3,10 @@ import * as shell from 'shelljs'
 export function logError(desc: string, suggestion: string, ref?: string) {
     console.log('❌', desc)
     console.log(suggestion)
-    console.log(ref)
-
+    if (ref) {console.log(ref)} else {console.log('Report this issue => https://github.com/openline-ai/openline-cli/issues/new/choose') }
+    shell.exec('rm -r openline-setup', {silent: true})
     console.log('')
     console.log('🦦 shutting down dev server...')
-    shell.exec('colima delete -f')
+    shell.exec('colima delete -f', {silent: true})
+    
 }
