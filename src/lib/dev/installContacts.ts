f989695e03b2f81ccb7a08995a670090cecca8b0
Eduard Firut <edi@openline.ai>
2022-11-24 21:27:35 +0200

    Set correct files for deployment

--- src/lib/dev/installContacts.ts
+++ src/lib/dev/installContacts.ts
@@ -16,17 +16,12 @@ function getSetupFiles(verbose :boolean, imageVersion: string = 'latest') :boole
 
     let dir = shell.exec('mkdir openline-setup')
 
-    let f1 = grabFile(config.contacts.apiDeployment, 'openline-setup/contacts-api-deployment.yaml', verbose)
-    let f2 = grabFile(config.contacts.apiService, 'openline-setup/contacts-api-service.yaml', verbose)
-    let f3 = grabFile(config.contacts.apiLoadbalancer, 'openline-setup/contacts-api-loadbalancer.yaml', verbose)
-    let f4 = grabFile(config.contacts.guiDeployment, 'openline-setup/contacts-gui-deployment.yaml', verbose)
-    let f5 = grabFile(config.contacts.guiService, 'openline-setup/contacts-gui-service.yaml', verbose)
-    let f6 = grabFile(config.contacts.guiLoadbalancer, 'openline-setup/contacts-gui-loadbalancer.yaml', verbose)
+    let f1 = grabFile(config.contacts.guiDeployment, 'openline-setup/contacts-gui-deployment.yaml', verbose)
+    let f2 = grabFile(config.contacts.guiService, 'openline-setup/contacts-gui-service.yaml', verbose)
 
     if (imageVersion != 'latest') {
         const options = {
             files: [
-                './openline-setup/contacts-api-deployment.yaml',
                 './openline-setup/contacts-gui-deployment.yaml'
             ],
             from: 'latest',
@@ -48,23 +43,11 @@ function contactsInstall(verbose :boolean, imageVersion :string = 'latest') :boo
     let result = true
     let config = getConfig()
 
-    // deploy Contacts API container image
-    let apiImage = config.contacts.apiImage.concat(imageVersion)
-    let apiDeployFile = './openline-setup/contacts-api-deployment.yaml'
-    let apiServiceFile = './openline-setup/contacts-api-service.yaml'
-    let apiLbFile = './openline-setup/contacts-api-loadbalancer.yaml'
-    let apiDeploy = deployImage(apiImage, apiDeployFile, apiServiceFile, apiLbFile, verbose)
-    if (!apiDeploy) {
-        error.logError('Error loading image', 'Unable to deploy Contacts API')
-        return false
-    }
-
     // deploy Contacts GUI container image
     let guiImage = config.contacts.guiImage.concat(imageVersion)
-    let guiDeployFile = ''
-    let guiServiceFile = ''
-    let guiLbFile = ''
-    let guiDeploy = deployImage(guiImage, guiDeployFile, guiServiceFile, guiLbFile, verbose)
+    let guiDeployFile = './openline-setup/contacts-gui-deployment.yaml'
+    let guiServiceFile = './openline-setup/contacts-gui-service.yaml'
+    let guiDeploy = deployImage(guiImage, guiDeployFile, guiServiceFile, verbose)
     if (!guiDeploy) {
         error.logError('Error loading image', 'Unable to deploy Contacts GUI')
         return false
