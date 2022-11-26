41a4405d42aae9c07f03bbbc62b892bb8d201602
mattbr0wn <matt@openline.ai>
2022-11-26 12:37:04 +0000

    linting

--- src/lib/dev/deploy.ts
+++ src/lib/dev/deploy.ts
@@ -1,24 +1,49 @@
 import * as shell from 'shelljs'
 import * as error from './errors'
 
-export function deployImage(imageUrl :string, deployYaml :string, serviceYaml :string, loadbalancerYaml? :string, verbose :boolean = false) :boolean {
-    let result = true
-    let namespace = 'openline'
+export interface Yaml {
+    deployYaml: string,
+    serviceYaml: string,
+    loadbalancerYaml?: string
+}
 
-    let pull = shell.exec(`docker pull ${imageUrl}`, {silent: !verbose})
-    if (pull.code != 0) {
-        error.logError(pull.stderr, `Unable to pull image ${imageUrl}`)
-        return false
+export function grabFile(fileLocation: string, setupPath: string, verbose :boolean) :boolean {
+  const result = true
+  const file = shell.exec(`curl -sS ${fileLocation} -o ${setupPath}`, {silent: !verbose})
+  if (file.code !== 0) {
+    error.logError(file.stderr, `Could not download setup file from ${fileLocation}`)
+    return false
+  }
+
+  return result
+}
+
+export function deployImage(imageUrl :string, deployConfig :Yaml, verbose = false) :boolean {
+  const result = true
+  const NAMESPACE = 'openline'
+
+  const pull = shell.exec(`docker pull ${imageUrl}`, {silent: !verbose})
+  if (pull.code !== 0) {
+    error.logError(pull.stderr, `Unable to pull image ${imageUrl}`)
+    return false
+  }
+
+  const deploy = shell.exec(`kubectl apply -f ${deployConfig.deployYaml} --namespace ${NAMESPACE}`, {silent: !verbose})
+  if (deploy.code !== 0) {
+    return false
+  }
+
+  const service = shell.exec(`kubectl apply -f ${deployConfig.serviceYaml} --namespace ${NAMESPACE}`, {silent: !verbose})
+  if (service.code !== 0) {
+    return false
+  }
+
+  if (deployConfig.loadbalancerYaml !== null) {
+    const lb = shell.exec(`kubectl apply -f ${deployConfig.loadbalancerYaml} --namespace ${NAMESPACE}`, {silent: !verbose})
+    if (lb.code !== 0) {
+      return false
     }
+  }
 
-    let deploy = shell.exec(`kubectl apply -f ${deployYaml} --namespace ${namespace}`, {silent: !verbose})
-    if (deploy.code != 0) {return false}
-    let service = shell.exec(`kubectl apply -f ${serviceYaml} --namespace ${namespace}`, {silent: !verbose})
-    if (deploy.code != 0) {return false}
-    if (loadbalancerYaml != null) {
-        let lb = shell.exec(`kubectl apply -f ${loadbalancerYaml} --namespace ${namespace}`, {silent: !verbose})
-        if (lb.code != 0) {return false}
-    }
-
-    return result
+  return result
 }
