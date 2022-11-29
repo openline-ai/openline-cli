540c7f262f5fd05be0770b9541f530b68c7b3771
mattbr0wn <matt@openline.ai>
2022-11-29 20:22:40 +0000

    delete commands

--- src/commands/dev/delete.ts
+++ src/commands/dev/delete.ts
@@ -1,4 +1,7 @@
 import {Command, Flags} from '@oclif/core'
+import * as shell from 'shelljs'
+import {deleteApp} from '../../lib/dev/delete'
+import {logError} from '../../lib/dev/errors'
 
 export default class DevDelete extends Command {
   static description = 'describe the command here'
@@ -8,21 +11,68 @@ export default class DevDelete extends Command {
   ]
 
   static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
+    all: Flags.boolean({char: 'a', description: 'delete all Openline services'}),
+    verbose: Flags.boolean({char: 'v'}),
   }
 
-  static args = [{name: 'file'}]
+  static args = [
+    {
+      name: 'app',
+      required: false,
+      description: 'the Openline app you would like to delete',
+      default: 'customer-os',
+      options: ['customer-os', 'contacts', 'oasis'],
+    },
+  ]
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(DevDelete)
+    let deployments :string[] = []
+    let services :string[] = []
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/mbrown/Code/openline/openline-cli/src/commands/dev/delete.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    if (flags.all) {
+      const reset = shell.exec('colima kubernetes reset', {silent: !flags.verbose})
+      const stop = shell.exec('colima stop')
+      if (reset.code === 0 && stop.code === 0) {
+        console.log('✅ Openline dev server deleted')
+      } else {
+        logError('Problem deleting Openline dev server', 'Let\'s nuke it from orbit...')
+      }
+    } else {
+      if (args.app.toLowerCase() === 'customer-os') {
+        deployments = ['customer-os-api', 'message-store', 'fusionauth-customer-os']
+        services = [
+          'neo4j-customer-os',
+          'neo4j-customer-os-admin',
+          'postgresql-customer-os-dev-hl',
+          'postgresql-customer-os-dev',
+          'fusionauth-customer-os',
+          'neo4j-customer-os-neo4j',
+          'customer-os-api-service',
+          'customer-os-api-loadbalancer',
+          'message-store-service',
+          'message-store-loadbalancer-service',
+        ]
+      } else if (args.app.toLowerCase() === 'contacts') {
+        deployments = ['contacts-gui']
+        services = ['contacts-gui-service', 'contacts-gui-loadbalancer']
+      } else if (args.app.toLowerCase === 'oasis') {
+        deployments = ['oasis-api', 'channels-api', 'oasis-frontend']
+        services = [
+          'oasis-api-service',
+          'oasis-api-loadbalancer',
+          'channels-api-service',
+          'channels-api-loadbalancer',
+          'oasis-frontend-service',
+          'oasis-frontend-loadbalancer',
+        ]
+      }
+
+      const deleted = deleteApp(deployments, services, flags.verbose)
+      if (deleted) {
+        console.log(`✅ ${args.app} deleted successfully`)
+      }
     }
   }
 }
+
--- src/lib/dev/delete.ts
+++ src/lib/dev/delete.ts
@@ -1,16 +1,23 @@
 import * as shell from 'shelljs'
+import * as error from '../../lib/dev/errors'
 
-export function deleteService(deployments: string[], services: string[], verbose: boolean) :boolean {
+export function deleteApp(deployments: string[], services: string[], verbose: boolean) :boolean {
   const NAMESPACE = 'openline'
 
   for (const deployment of deployments) {
     const result = shell.exec(`kubectl delete deployments ${deployment} -n ${NAMESPACE}`, {silent: !verbose})
-    if (result.code !== 0) return false
+    if (result.code !== 0) {
+      error.logError(result.stderr, 'Problem deleting deployment')
+      return false
+    }
   }
 
   for (const service of services) {
     const result = shell.exec(`kubectl delete service ${service} -n ${NAMESPACE}`, {silent: !verbose})
-    if (result.code !== 0) return false
+    if (result.code !== 0) {
+      error.logError(result.stderr, 'Problem deleting service')
+      return false
+    }
   }
 
   return true
