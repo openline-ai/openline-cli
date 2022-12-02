89b79ed759360e3c0699fbfa9a07324b755d96f7
mattbr0wn <matt@openline.ai>
2022-12-02 17:23:01 +0000

    added colors

--- src/commands/dev/ping.ts
+++ src/commands/dev/ping.ts
@@ -3,6 +3,7 @@ import {pingFusionAuth} from '../../lib/dev/auth'
 import {pingContactsGui} from '../../lib/dev/contacts'
 import {pingCustomerOsApi, pingMessageStoreApi} from '../../lib/dev/customer-os'
 import {pingOasisGui, pingOasisApi, pingChannelsApi} from '../../lib/dev/oasis'
+import * as colors from 'colors' // eslint-disable-line no-restricted-imports
 const Table = require('cli-table') // eslint-disable-line unicorn/prefer-module
 
 export default class DevPing extends Command {
@@ -19,21 +20,21 @@ export default class DevPing extends Command {
   public async run(): Promise<void> {
     // const {flags} = await this.parse(DevPing)
 
-    // const auth = pingFusionAuth() ? '✅' : '❌'
-    const contactsGui = pingContactsGui() ? '✅' : '❌'
-    const customerOsApi = pingCustomerOsApi() ? '✅' : '❌'
-    const messageStoreApi = pingMessageStoreApi() ? '✅' : '❌'
-    const channelsApi = pingChannelsApi() ? '✅' : '❌'
-    const oasisApi = pingOasisApi() ? '✅' : '❌'
-    const oasisGui = pingOasisGui() ? '✅' : '❌'
+    const auth = pingFusionAuth() ? colors.bold.green('Yes') : colors.red.bold('No')
+    const contactsGui = pingContactsGui() ? colors.bold.green('Yes') : colors.red.bold('No')
+    const customerOsApi = pingCustomerOsApi() ? colors.bold.green('Yes') : colors.red.bold('No')
+    const messageStoreApi = pingMessageStoreApi() ? colors.bold.green('Yes') : colors.red.bold('No')
+    const channelsApi = pingChannelsApi() ? colors.bold.green('Yes') : colors.red.bold('No')
+    const oasisApi = pingOasisApi() ? colors.bold.green('Yes') : colors.red.bold('No')
+    const oasisGui = pingOasisGui() ? colors.bold.green('Yes') : colors.red.bold('No')
 
     const table = new Table({
-      head: ['App', 'Service', 'Port', 'Reachable?'],
-      // colWidths: [100, 100, 100]
+      head: [colors.cyan.bold('App'), colors.cyan.bold('Service'), colors.cyan.bold('Port'), colors.cyan.bold('Up?')],
+      colWidths: [15, 20, 10, 5],
     })
 
     table.push(
-      // ['customerOs', 'auth', auth],
+      ['customerOs', 'auth', '9011', auth],
       ['customer-os', 'customer-os-api', '10000', customerOsApi],
       ['customer-os', 'message-store-api', '9009', messageStoreApi],
       ['contacts', 'contacts-gui', '3000', contactsGui],
