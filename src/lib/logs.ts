2adadde7c95132f6be6bc53b916e553b1d12f7a5
mattbr0wn <matt@openline.ai>
2022-12-02 22:00:27 +0000

    dev clone

---
+++ src/lib/logs.ts
@@ -0,0 +1,18 @@
+import * as colors from 'colors' // eslint-disable-line no-restricted-imports
+
+export function logTerminal(type: string, description: string, location?: string) :void {
+  if (type.toUpperCase() === 'ERROR') {
+    type = colors.red.bold(`[${type.toUpperCase()}]`)
+  } else if (type.toUpperCase() === 'SUCCESS') {
+    type = '🦦 '
+  } else {
+    type = colors.cyan(`[${type.toUpperCase()}]`)
+  }
+
+  console.log(type, description)
+
+  if (location) {
+    location = colors.italic(location)
+    console.log(location)
+  }
+}
