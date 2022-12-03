6afd2d05f35a75647463dea82da1f415985d79ec
mattbr0wn <matt@openline.ai>
2022-12-03 20:17:32 +0000

    update logging

--- src/lib/dev/build-image.ts
+++ src/lib/dev/build-image.ts
@@ -1,13 +1,13 @@
 import * as shell from 'shelljs'
-import * as error from './errors'
+import {logTerminal} from '../logs'
 
 export function buildLocalImage(path: string, imageName: string, verbose: boolean) :boolean {
   const dockerBuild = `docker build -t ${imageName} -f ${path}/${imageName}/Dockerfile ${path}`
-  if (verbose) console.log(`[EXEC] ${dockerBuild}`)
+  if (verbose) logTerminal('EXEC', dockerBuild)
 
   const buildExecution = shell.exec(dockerBuild, {silent: !verbose})
   if (buildExecution.code !== 0) {
-    error.logError(buildExecution.stderr, `Unable to build image in ${path}`, true)
+    logTerminal('ERROR', buildExecution.stderr, 'dev:build-image:buildLocalImage')
     return false
   }
 
