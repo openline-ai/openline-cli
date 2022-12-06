e75e081065e58e264b18777b8a83b7144493c0a6
tsearle <torrey@openline.ai>
2022-12-06 10:54:47 +0100

    add missing import

--- src/lib/dev/build-image.ts
+++ src/lib/dev/build-image.ts
@@ -1,5 +1,7 @@
 import * as shell from 'shelljs'
 import {logTerminal} from '../logs'
+import { getPlatform } from '../dependencies'
+
 
 export function buildLocalImage(path: string, context: string, imageName: string, verbose: boolean) :boolean {
   const dockerBuild = `docker build -t ${imageName} -f ${path}/Dockerfile ${context}`
