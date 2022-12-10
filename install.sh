69831bfe2ea9bbf4b302cc79364f6fb559521ecc
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2022-12-10 08:11:54 +0000

    Add files via upload (#117)

---
+++ install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+{
+    set -e
+    SUDO=''
+    if [ "$(id -u)" != "0" ]; then
+      SUDO='sudo'
+      echo "This script requires superuser access."
+      echo "You will be prompted for your password by sudo."
+      # clear any previous sudo permission
+      sudo -k
+    fi
+
+
+    # run inside sudo
+    $SUDO bash <<SCRIPT
+  set -e
+
+  echoerr() { echo "\$@" 1>&2; }
+
+  if [[ ! ":\$PATH:" == *":/usr/local/bin:"* ]]; then
+    echoerr "Your path is missing /usr/local/bin, you need to add this to use this installer."
+    exit 1
+  fi
+
+  if [ "\$(uname)" == "Darwin" ]; then
+    OS=darwin
+  elif [ "\$(expr substr \$(uname -s) 1 5)" == "Linux" ]; then
+    OS=linux
+  else
+    echoerr "This installer is only supported on Linux and MacOS"
+    exit 1
+  fi
+
+  ARCH="\$(uname -m)"
+  if [ "\$ARCH" == "x86_64" ]; then
+    ARCH=x64
+  elif [[ "\$ARCH" == aarch* ]]; then
+    ARCH=arm
+  elif [[ "\$ARCH" == arm64 ]]; then
+    ARCH=arm
+  else
+    echoerr "unsupported arch: \$ARCH"
+    exit 1
+  fi
+
+  mkdir -p /usr/local/lib
+  cd /usr/local/lib
+  rm -rf openline
+  if [ \$(command -v xz) ]; then
+    URL=http://openline.sh/openline-\$OS-\$ARCH.tar.xz
+    TAR_ARGS="xJ"
+  else
+    URL=https://openline.sh/openline-\$OS-\$ARCH.tar.gz
+    TAR_ARGS="xz"
+  fi
+  echo "Installing CLI from \$URL"
+  if [ \$(command -v curl) ]; then
+    curl "\$URL" | tar "\$TAR_ARGS"
+  else
+    wget -O- "\$URL" | tar "\$TAR_ARGS"
+  fi
+  # delete old openline bin if exists
+  rm -f \$(command -v openline) || true
+  rm -f /usr/local/bin/openline
+  ln -s /usr/local/lib/openline/bin/openline /usr/local/bin/openline
+
+  # on alpine (and maybe others) the basic node binary does not work
+  # remove our node binary and fall back to whatever node is on the PATH
+  /usr/local/lib/openline/bin/node -v || rm /usr/local/lib/openline/bin/node
+
+SCRIPT
+  # test the CLI
+  LOCATION=$(command -v openline)
+  echo "openline installed to $LOCATION"
+  openline --version
+}
