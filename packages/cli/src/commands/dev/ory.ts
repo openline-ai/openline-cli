775c0f849cb468d812fdf233beffa6081d4d279d
tsearle <torrey@openline.ai>
2023-03-24 10:05:48 +0100

    fix ory-tunnel

--- packages/cli/src/commands/dev/ory.ts
+++ packages/cli/src/commands/dev/ory.ts
@@ -3,6 +3,8 @@ import {getConfig} from '../../config/dev'
 import {installOryTunnel} from '../../lib/dev/customer-os'
 const { spawn } = require('child_process')
 import {logTerminal} from '../../lib/logs'
+import {cloneRepo} from '../../lib/clone/clone-repo'
+import { cleanupSetupFiles } from '../../lib/dev/start'
 
 
 
@@ -36,7 +38,10 @@ export default class DevOry extends Command {
     switch (action) {
       case 'tunnel':
         logTerminal('INFO', 'Starting Ory Tunnel...')
+        cleanupSetupFiles()
+        cloneRepo(config.customerOs.repo, flags.verbose, config.setupDir, undefined, true)
         installOryTunnel(config.customerOs.repo, flags.verbose)
+        cleanupSetupFiles()
         const shell = spawn('kubectl',['-n', 'openline', 'exec', '-it', 'ory-tunnel-0', '--', 'tunnel.sh'], { stdio: 'inherit', shell: true })
         break
     }
