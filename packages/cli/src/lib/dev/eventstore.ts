7c208c338123f297afad243d763727e96c937e80
SilviuOpenline <125381623+SilviuOpenline@users.noreply.github.com>
2023-10-04 22:54:10 +0300

    318 event store for linux (#319)
    * [318] - event store for linux
    
    * [318] - event store for linux
    
    * [318] - event store for linux

--- packages/cli/src/lib/dev/eventstore.ts
+++ packages/cli/src/lib/dev/eventstore.ts
@@ -2,6 +2,9 @@ import {getConfig} from "../../config/dev";
 import * as shell from "shelljs";
 import {deployImage, Yaml, updateImageTag} from './deploy'
 import {logTerminal} from "../logs";
+import {getPlatform} from "../dependencies";
+import * as colima from "./colima";
+import * as k3d from "./k3d";
 
 
 const config = getConfig()
@@ -12,12 +15,20 @@ function postgresqlServiceCheck() :boolean {
   return (shell.exec(`kubectl get service ${EVENTSTORE_SERVICE} -n ${NAMESPACE}`, {silent: true}).code === 0)
 }
 
-export function installEventStoreDB(verbose: boolean, location = config.setupDir, imageVersion = 'latest') :boolean {
+export function installEventStoreDB(verbose: boolean, location = config.setupDir) :boolean {
   const DEPLOYMENT = location + config.customerOs.eventStoreDbDeployment
   const SERVICE = location + config.customerOs.eventStoreDbService
   const LOADBALANCER = location + config.customerOs.eventStoreDbLoadbalancer
-
-  let image: string | null = config.customerOs.eventStoreDbImage
+  let IMAGE_VERSION
+  switch (getPlatform()) {
+    case 'mac':
+      IMAGE_VERSION = "22.10.3-alpha-arm64v8"
+      break
+    case 'linux':
+      IMAGE_VERSION = "22.10.3-bionic"
+      break
+  }
+  let image: string | null = config.customerOs.eventStoreDbImage+IMAGE_VERSION
 
   const installConfig: Yaml = {
     deployYaml: DEPLOYMENT,
