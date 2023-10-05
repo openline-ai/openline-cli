7269995a0564e4e7d5fe9fbeb4f0d08cf28df3cb
SilviuOpenline <125381623+SilviuOpenline@users.noreply.github.com>
2023-10-05 14:56:46 +0300

    318 event store for linux (#320)
    * [318] - event store for linux
    
    * [318] - event store for linux
    
    * [318] - event store for linux
    
    * [318] - event store for linux
    
    * [318] - event store for linux
    
    * [318] - event store for linux
    
    * [318] - event store for linux
    
    * [318] - event store for linux

--- packages/cli/src/lib/dev/eventstore.ts
+++ packages/cli/src/lib/dev/eventstore.ts
@@ -20,14 +20,19 @@ export function installEventStoreDB(verbose: boolean, location = config.setupDir
   const SERVICE = location + config.customerOs.eventStoreDbService
   const LOADBALANCER = location + config.customerOs.eventStoreDbLoadbalancer
   let IMAGE_VERSION
+  let ImageUpdate
   switch (getPlatform()) {
     case 'mac':
-      IMAGE_VERSION = "22.10.3-alpha-arm64v8"
+      IMAGE_VERSION = config.customerOs.eventStoreMacImageVersion
       break
     case 'linux':
-      IMAGE_VERSION = "22.10.3-bionic"
+      IMAGE_VERSION = config.customerOs.eventStoreLinuxImageVersion
       break
   }
+
+  ImageUpdate = 'sed "s/\\$IMAGE_VERSION/' + IMAGE_VERSION + `/" `+ DEPLOYMENT + ` > event-store-temp.yaml && mv event-store-temp.yaml ` + DEPLOYMENT
+  shell.exec(ImageUpdate, {silent: !verbose})
+
   let image: string | null = config.customerOs.eventStoreDbImage+IMAGE_VERSION
 
   const installConfig: Yaml = {
