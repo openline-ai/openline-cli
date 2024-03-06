0ee304cda3ac8c30b27a51bc829dc81d64a35e94
max <151522527+max-openline@users.noreply.github.com>
2024-03-06 11:22:16 +0100

    [refactor] - refactor temporal stack logic (#385)
    * bugfix - fix temporal selector
    
    * refactor temporal deployment
    
    * remove unnecessary docker compose
    
    * temporal ping refactor
    
    * logging extra changes

--- packages/cli/src/lib/logs.ts
+++ packages/cli/src/lib/logs.ts
@@ -1,13 +1,17 @@
 import * as colors from 'colors' // eslint-disable-line no-restricted-imports
 
-export function logTerminal(type: string, description: string, location?: string) :void {
+export function logTerminal(type: string, description: string, location?: string): void {
   if (type.toUpperCase() === 'ERROR') {
     type = colors.red.bold(`[${type.toUpperCase()}]`)
   } else if (type.toUpperCase() === 'SUCCESS') {
     type = '🦦 '
+    description = colors.green(description)
   } else if (type.toUpperCase() === 'EXEC') {
     type = colors.grey(`[${type.toUpperCase()}]`)
     description = colors.grey(description)
+  } else if (type.toUpperCase() === 'WARNING') {
+    type = colors.yellow(`[${type.toUpperCase()}]`)
+    description = colors.yellow(description)
   } else {
     type = colors.cyan(`[${type.toUpperCase()}]`)
   }
