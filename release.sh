dd2312184b4d485564768eca175263fe908c1fd0
max-openline <max@openline.ai>
2024-03-06 17:44:19 +0100

    cleanup file

--- release.sh
+++ release.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#Bump the version in .../openline-cli/packages/cli/package.json
 
-# Go to: .../openline-cli/packages/cli and run:
+# Install node and npm
+# Install oclif (npm i -g oclif)
+# Install aws-cli (brew install awscli)
+# Install 7zip (brew install p7zip)
+# Bump the version in .../openline-cli/packages/cli/package.json
+
 cd packages/cli
 oclif pack tarballs
 
@@ -12,18 +16,18 @@ upload_out=$(oclif upload tarballs)
 version=$(echo $upload_out | perl -pe 'if(($v)=/([0-9]+([.][0-9]+)+)/){print"$v\n";exit}$_=""')
 sha=$(echo $upload_out | tr -d '\n' | rev | cut -d "-" -f1 | rev)
 
-# - —> Output: oclif: done uploading tarballs & manifests for 0.7.13-5157b363 —>>> get the version and the sha from the Output and fill in the next command - the version is to be used without the v prefix
  
 oclif promote --version "${version}" --sha "${sha}" --channel rc
-       # - —> Downloadable from http://openline.sh/channels/rc/openline-darwin-arm64.tar.gz
 
-# .../openline-cli/packages/cli 
 oclif promote --version "${version}" --sha "${sha}" --channel stable --indexes
 
 # Check the new version availability: 
 openline update --available
+
+# Download the tarball and get the sha256
 aws s3 cp s3://openline.sh/channels/stable/openline-darwin-arm64.tar.gz .
 shasum -a 256 openline-darwin-arm64.tar.gz
+# clean up
 rm openline-darwin-arm64.tar.gz
 
 echo "go the homebrew formula in github and update the sha256 value in line 12." 
